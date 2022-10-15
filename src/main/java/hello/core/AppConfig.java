81ffac385a2f4ba358d0c8f3a6c7c873946b6394
tenta <eunoia3802@gmail.com>
2022-10-15 19:03:38 +0900

    스프링 컨테이너 등록 설정: @Configuration, @Bean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -7,20 +7,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public  MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
+    @Bean
     public OrderService orderService(){ //생성자 주입 (DI 의존관계 주입)
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
