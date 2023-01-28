2bfd1704e14de7484f9a982295258fabb1af3097
tenta <eunoia3802@gmail.com>
2023-01-28 21:24:02 +0900

    컴포넌트 스캔과 의존관계 자동 주입 확인 테스트 구현

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,16 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // basePackages 에서 지정한 하위 클래스에서부터 찾음
+        // basePackages = "hello.core.member",
+        // basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
